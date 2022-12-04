2515a8aaa0f52f844565689c5c7999f6589e2aa6
Dimi Chakarov <dimitar@dchakarov.com>
2022-12-04 14:50:25 +0000

    part 1 & 2

--- Advent of Code/main.swift
+++ Advent of Code/main.swift
@@ -13,27 +13,46 @@ func main() {
     let lines = inputString.components(separatedBy: "\n")
         .filter { !$0.isEmpty }
     
-    // Sample algorithm
-    var scoreboard = [String: Int]()
+    var fullyContainCount = 0
+    var partiallyOverlapCount = 0
     lines.forEach { line in
-        let (name, score) = parseLine(line)
-        scoreboard[name] = score
+        let elves = parseLine(line)
+        let fullyContain = fullyContain(first: elves.first, second: elves.second)
+        if fullyContain { fullyContainCount += 1 }
+        let partiallyOverlap = partiallyOverlap(first: elves.first, second: elves.second)
+        if partiallyOverlap { partiallyOverlapCount += 1 }
     }
-    scoreboard
-        .sorted { lhs, rhs in
-            lhs.value > rhs.value
-        }
-        .forEach { name, score in
-            print("\(name) \(score) pts")
-        }
+    
+    print(fullyContainCount)
+    print(partiallyOverlapCount)
 }
 
-func parseLine(_ line: String) -> (name: String, score: Int) {
-    let helper = RegexHelper(pattern: #"([\-\w]*)\s(\d+)"#)
-    let result = helper.parse(line)
-    let name = result[0]
-    let score = Int(result[1])!
-    return (name: name, score: score)
+func parseLine(_ line: String) -> (first: (from: Int, to: Int),
+                                   second: (from: Int, to: Int)) {
+    let chunks = line.components(separatedBy: ",")
+    let firstChunks = chunks[0].components(separatedBy: "-")
+    let secondChunks = chunks[1].components(separatedBy: "-")
+    
+    return (first: (from: Int(firstChunks[0])!, to: Int(firstChunks[1])!),
+            second: (from: Int(secondChunks[0])!, to: Int(secondChunks[1])!))
+}
+
+func fullyContain(first: (from: Int, to: Int),
+                  second: (from: Int, to: Int)) -> Bool {
+    if first.from <= second.from && first.to >= second.to { return true }
+    if second.from <= first.from && second.to >= first.to { return true }
+    return false
+}
+
+func partiallyOverlap(first: (from: Int, to: Int),
+                      second: (from: Int, to: Int)) -> Bool {
+    if first.from <= second.from && first.to >= second.to { return true }
+    if second.from <= first.from && second.to >= first.to { return true }
+    
+    if first.from <= second.from && first.to >= second.from { return true }
+    if second.from <= first.from && second.to >= first.from { return true }
+    
+    return false
 }
 
 main()
